f961dd588a83a12e37aa040ee69b9ddd692f19f5
wooihaw <wooihaw.tan@gmail.com>
2023-07-25 14:26:40 +0800

    Updated with solutions

--- exercises/02_list_exercises/01_multiply_all.py
+++ exercises/02_list_exercises/01_multiply_all.py
@@ -1,3 +1,8 @@
 # Write a Python script to multiply all the items in a list.
 alist = [12, 43, 57, 98, 83, -103, 256, -1]
 print(f'{alist=}')
+
+ans = 1
+for i in alist:
+    ans = ans * i  # ans *= i
+print(ans)
--- exercises/02_list_exercises/04_even_numbers.py
+++ exercises/02_list_exercises/04_even_numbers.py
@@ -1,3 +1,14 @@
 # Write a Python script that takes a list of numbers and returns a new list containing only the even numbers from the original list.
 alist = [12, 43, 57, 98, 83, -103, 256, -55, 168, -6]
 print(f'{alist=}')
+
+# Use for loop
+blist = []
+for i in alist:
+    if i % 2 == 0:
+        blist.append(i)
+print(f"{blist = }")
+
+# Use list comprehension
+clist = [i for i in alist if i%2 == 0]
+print(f"{clist = }")
